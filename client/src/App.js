7f685452c2eb8bd54b8c35e50986a63227316f22
dimple23 <dimplechavda23@gmail.com>
2019-06-01 14:55:47 -0400

    save

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Stocks from './pages/Stocks';
 import User from './pages/User';
-import AppNavbar from './components/AppNavbar';
-//import StockChart from './components/StockChart';
-import StockNews from './components/StockNews';
+
 
 function App() {
   return (
@@ -17,17 +15,6 @@ function App() {
           <Route render={() => <h1> 404 Error</h1>} />
         </Switch>
       </div>
-      <div className="App">
-					<header className="App-header">
-						<AppNavbar />
-					</header>
-					<section className="stock-panel">
-					
-						<section className="info-section">
-							<StockNews />
-						</section>
-					</section>
-				</div>
     </Router>
   );
 }
