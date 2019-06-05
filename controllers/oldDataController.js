e3605ba1bc994a1d950b501b0f2af398415e7067
fjamik <fjamik@gmail.com>
2019-06-05 13:37:56 -0400

    update

--- controllers/oldDataController.js
+++ controllers/oldDataController.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 function quandlStock(req, res) {
   console.log ('get all stock in reall')
-  axios.get(`https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=${process.env.quandlapi}`)
+  axios.get(`https://www.quandl.com/api/v3/datasets/NASDAQOMX/XQC?start_date=2019-05-31&end_date=2019-06-04&api_key=${process.env.quandlapi}`)
   .then(function(data){
     // console.log(data.data);
     res.json(data.data)
